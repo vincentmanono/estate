589214684d173e2580de9e62b4c741d0eb72e625
vincentmanono <vincentnyamongo95@gmail.com>
2020-11-01 14:03:32 +0300

    client side index page

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/',function(){
+    return view('client.index');
 });
 
 Auth::routes();
