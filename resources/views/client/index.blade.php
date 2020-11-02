d4072a9aef46a46edf033137814f2155275ffa8d
vincentmanono <vincentnyamongo95@gmail.com>
2020-11-02 18:03:29 +0300

    removed application form from index

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -20,58 +20,8 @@
 <div class="parallax first-section">
     <div class="container">
         <div class="row">
-            <div class="col-md-6 wow slideInLeft hidden-xs hidden-sm">
-                <div class="contact_form">
-                    <h3><i class="fa fa-envelope-o grd1 global-radius"></i>APARTMENT APPLICATION</h3>
-                    @include('messages')
-                     <form id="contactform1" class="row" action="" method="post">{{--  {{ route('request.application') }}  --}}
 
-                        @csrf()
-                        @method('POST')
-
-                        <fieldset class="row-fluid">
-                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                                <input type="text" name="name" id="first_name1" class="form-control" placeholder="name">
-                            </div>
-                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                                <select name="property_id" class="form-control" id="">
-                                    <option value="">-----select-property-----</option>
-                                    @foreach ($properties as $property)
-
-                                    <option value="{{ $property->id }}">{{ $property->name }}</option>
-
-                                    @endforeach
-                                </select>
-                             </div>
-                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                                <input type="email" name="email" id="email1" class="form-control" placeholder="Your Email">
-                            </div>
-                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                                <input type="text" name="phone" id="phone1" class="form-control" placeholder="Your Phone">
-                            </div>
-
-
-
-
-
-                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                                <input type="text" name="identity"  class="form-control" placeholder="Your ID">
-                            </div>
-                            {{--  <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                                <input type="text" name="phone1" id="phone1" class="form-control" placeholder="Your Phone">
-                            </div>  --}}
-
-
-
-
-                            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
-                                <button type="submit" value="SEND" id="submit1" class="btn btn-light btn-radius btn-brd grd1 btn-block">Apply</button>
-                            </div>
-                        </fieldset>
-                    </form>
-                </div>
-            </div>
-            <div class="col-md-6 col-sm-12">
+            <div class="col-md-8 col-sm-12">
                 <div class="big-tagline clearfix">
                     <h2>Chief Property Real Estates</h2>
                     <p class="lead">Welcome to Chief Properties Real Estate Site. </p>
