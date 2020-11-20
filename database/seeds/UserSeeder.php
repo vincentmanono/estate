3ed8e10c8d71b04775ac4cb40433bda3f4644514
Abrahamkivosh <abrahamkivosh@gmail.com>
2020-11-20 04:42:53 -0800

    seeding owner only

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -15,7 +15,7 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\User::class,10)->create();
+        // factory(App\User::class,10)->create();
         $name = "sokoro78";
 
         User::create([
