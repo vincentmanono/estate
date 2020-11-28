0ca99881f53830920515d27f31f70d49e401d384
vincentmanono <vincentnyamongo95@gmail.com>
2020-11-28 11:23:44 +0300

    chiefinvestlease

--- resources/views/chiefinvestmentlease.blade.php
+++ resources/views/chiefinvestmentlease.blade.php
@@ -38,7 +38,7 @@
                         <div class="card-body">
                             <div class="container-fluid">
                                 <div class="row">
-                                    <div class="col-md-12 ">
+                                    <div class="col-md-12">
                                         <h3>
                                             <b >
                                                 Chief Investment Company
